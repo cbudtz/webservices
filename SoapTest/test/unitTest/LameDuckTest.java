41a657db6723264701d8ddc87bf91151bc880f0f
Christian <chrbudtz@gmail.com>
2015-11-16 14:16:45 +0100

    More LameDuckTest

--- SoapTest/test/unitTest/LameDuckTest.java
+++ SoapTest/test/unitTest/LameDuckTest.java
@@ -5,18 +5,29 @@
  */
 package unitTest;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.DatatypeFactoryImpl;
+import java.util.jar.Pack200;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.datatype.DatatypeFactory;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.BookFlightFault;
+import org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.CancelFlightFault;
+import org.netbeans.xml.schema.lameduckelements.BookFlightRequestType;
+import org.netbeans.xml.schema.lameduckelements.CancelFlightRequestType;
+import org.netbeans.xml.schema.lameduckelements.CreditCardInfoType;
 
 /**
  *
  * @author Christian
  */
 public class LameDuckTest {
+    DatatypeFactory df = new DatatypeFactoryImpl();
     
     public LameDuckTest() {
     }
@@ -47,10 +58,90 @@ public class LameDuckTest {
     
     @Test 
     public void bookFlightTest(){
-        fail("Not implemented yet");
+        boolean failed = false;
+        try {
+            
+            failed = bookFlight(new BookFlightRequestType());
+            fail("Should have returned exception");
+        } catch (BookFlightFault ex) {
+            
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        BookFlightRequestType req = new BookFlightRequestType();
+        req.setBookingNumber(123456);
+        try {
+            bookFlight(req);
+        } catch (BookFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        req.setCreditcardInfo(new CreditCardInfoType());
+        try {
+            bookFlight(req);
+        } catch (BookFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     @Test
     public void cancelFlightTest(){
-        fail("Not implemented yet");
+        try {
+            cancelFlight(null);
+            fail("Should have returned a CancelFlightFault!");
+        } catch (CancelFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            cancelFlight(new CancelFlightRequestType());
+            fail("Should have returned a CancelFlightFault!");
+        } catch (CancelFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        CancelFlightRequestType req = new CancelFlightRequestType();
+        req.setBookingNumber(123456);
+        try {
+            cancelFlight(req);
+            fail("Should have thrown CancelFlightFault");
+        } catch (CancelFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        req.setCreditCardInformation(new CreditCardInfoType());
+        try {
+            cancelFlight(req);
+            fail("Should have thrown CancelFlightFault");
+        } catch (CancelFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        CreditCardInfoType cred = new CreditCardInfoType();
+                cred.setCardNumber(1234124);
+                cred.setExpirationDate(df.newXMLGregorianCalendar(2016, 1, 1, 1, 1, 1, 1, 1));
+                cred.setHolderName("Prins Valiant");
+        req.setCreditCardInformation(new CreditCardInfoType());
+        try {
+            cancelFlight(req);
+            fail("Should have thrown CancelFlightFault");
+        } catch (CancelFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        req.setPrice(1000000000);
+        try {
+            cancelFlight(req);
+        } catch (CancelFlightFault ex) {
+            Logger.getLogger(LameDuckTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+    }
+
+    private static boolean bookFlight(org.netbeans.xml.schema.lameduckelements.BookFlightRequestType flightInfo) throws BookFlightFault {
+        org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.LameDuckWSDLService service = new org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.LameDuckWSDLService();
+        org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        return port.bookFlight(flightInfo);
+    }
+
+    private static String cancelFlight(org.netbeans.xml.schema.lameduckelements.CancelFlightRequestType flightInfo) throws CancelFlightFault {
+        org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.LameDuckWSDLService service = new org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.LameDuckWSDLService();
+        org.netbeans.j2ee.wsdl.lameduck.wsdl.lameduckwsdl.LameDuckPortType port = service.getLameDuckPortTypeBindingPort();
+        return port.cancelFlight(flightInfo);
     }
 }
