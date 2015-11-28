274852cc2b28eae08e6bf25ae6e9cfad2b8e38c7
Christian <chrbudtz@gmail.com>
2015-11-28 16:24:07 +0100

    Created BookingExceptiuon to handle Errors

---
+++ TravelGoodREST/src/java/ws/group15/BookingException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.group15;
+
+/**
+ *
+ * @author Christian
+ */
+public class BookingException extends Exception {
+
+    public BookingException(String msg) {
+      super(msg);
+    }
+    
+    
+}
