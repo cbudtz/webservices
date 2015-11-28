b224a722c760624d683f82c10f47cac3097f6813
Christian <chrbudtz@gmail.com>
2015-11-28 17:26:56 +0100

    bookItinerary attempt....

--- TravelGoodREST/src/java/ws/group15/DataSingleton.java
+++ TravelGoodREST/src/java/ws/group15/DataSingleton.java
@@ -38,120 +38,147 @@ import ws.group15.dto.Itinerary;
  * @author Christian
  */
 public class DataSingleton {
+
     private static DataSingleton instance;
     private Hashtable<String, Itinerary> itineraries; //<userId, <itineraryId,Itinea
     private static LameDuckPortType lameDuckPort;
     private static NiceViewWSDLPortType niceViewPort;
-    
-    private DataSingleton(){
+
+    private DataSingleton() {
         itineraries = new Hashtable<>();
         LameDuckWSDLService lameDuckService = new LameDuckWSDLService();
         lameDuckPort = lameDuckService.getLameDuckPortTypeBindingPort();
         NiceViewWSDLService niceViewService = new NiceViewWSDLService();
         niceViewPort = niceViewService.getNiceViewWSDLPortTypeBindingPort();
     }
-    
-    
-    
-    public static synchronized DataSingleton getInstance(){
-        if (instance == null) instance = new DataSingleton();
+
+    public static synchronized DataSingleton getInstance() {
+        if (instance == null) {
+            instance = new DataSingleton();
+        }
         return instance;
     }
-    
+
     //POST in itineraries
-    public Itinerary createItinerary(){
+    public Itinerary createItinerary() {
         String itineraryID = UUID.randomUUID().toString();
         Itinerary it = new Itinerary();
         it.id = itineraryID;
         itineraries.put(itineraryID, it);
-        return it;   
+        return it;
     }
-    
+
     //GET on itineraries/{itId}
-    public Itinerary getItineraryById(String itineraryID){
+    public Itinerary getItineraryById(String itineraryID) {
         return itineraries.get(itineraryID);
     }
-    
+
     //GET on itineraries //Not part of requirements specification! Made for fun and practice
-    public List<Itinerary> getItineraries(){
+    public List<Itinerary> getItineraries() {
         ArrayList<Itinerary> itineraryList = new ArrayList<>();
         for (Map.Entry<String, Itinerary> entrySet : itineraries.entrySet()) {
-                itineraryList.add(entrySet.getValue());
-            }
+            itineraryList.add(entrySet.getValue());
+        }
         return itineraryList;
     }
-    
-    public boolean setCreditCard(CreditCardInfo creditCard, String itineraryID){
+
+    public boolean setCreditCard(CreditCardInfo creditCard, String itineraryID) {
         Itinerary it = itineraries.get(itineraryID);
-        if (it == null) return false;
+        if (it == null) {
+            return false;
+        }
         it.creditCard = creditCard;
         return true;
     }
-    
-    public boolean bookItinerary(String itineraryID) throws BookingException{
+
+    public boolean bookItinerary(String itineraryID) throws BookingException {
         Itinerary it = itineraries.get(itineraryID);
-        if (it ==null) throw new BookingException("No such Itinerary!");
-        if (it.creditCard == null) throw  new BookingException("No creditcard entered");
+        if (it == null) {
+            throw new BookingException("No such Itinerary!");
+        }
+        if (it.creditCard == null) {
+            throw new BookingException("No creditcard entered");
+        }
         //try to book flights!
+        boolean succes = true;
+
         for (FlightInformation flight : it.flights) {
             BookFlightRequestType req = createRequestFromFlightInfo(flight, it.creditCard);
             try {
                 lameDuckPort.bookFlight(req);
+                flight.bookingState = Itinerary.BookingState.PAID;
             } catch (BookFlightFault ex) {
-               //TODO: something useful with fault;
+                //TODO: something useful with fault;
+                succes = false;
                 System.out.println("Booking failed");
-            }            
-        }
-        for (HotelInformation hotel : it.hotels){
-            BookHotelRequestType req = createRequestFromHotelInfo(hotel, it.creditCard);
-            try {
-                niceViewPort.bookHotel(req);
-            } catch (BookHotelFault ex) {
-                //TODO something useful with try catch
-                System.out.println("Booking failed");
-                
+                boolean compensationSucces = true; //compensate(it);
+                if (compensationSucces) {
+                    throw new BookingException("Booking failed - Money refunded");
+                } else {
+                    throw new BookingException("Booking failed - Compensation failed :(");
+                }
             }
         }
-        
-        
+        if (succes) { //everything went well with the flight - now try the hotels
+            for (HotelInformation hotel : it.hotels) {
+                BookHotelRequestType req = createRequestFromHotelInfo(hotel, it.creditCard);
+                try {
+                    niceViewPort.bookHotel(req);
+                    hotel.state = Itinerary.BookingState.PAID;
+                } catch (BookHotelFault ex) {
+                    //TODO something useful with try catch
+                    System.out.println("Booking failed");
+                    boolean compensationSucces = true; //compensate(it);
+                    if (compensationSucces) {
+                        throw new BookingException("Booking failed - Money refunded");
+                    } else {
+                        throw new BookingException("Booking failed - Compensation failed :(");
+                    }
+                }
+            }
+        }
+
         return true;
     }
-    
-    public boolean cancelItinerary(String itineraryID){
+
+    public boolean cancelItinerary(String itineraryID) {
         //TODO lots of magic to cancel itinerary - return true if everyThing went well
         return true;
     }
+
     //PUT on flights
-    public void addFlightToItinerary(String itineraryID, FlightInformation flightInfo){
+    public void addFlightToItinerary(String itineraryID, FlightInformation flightInfo) {
         itineraries.get(itineraryID).flights.add(flightInfo);
     }
-    
+
     //PUT on hotels
-    public void addHotelToItinerary(String itineraryID, HotelInformation hotelInformation){
+    public void addHotelToItinerary(String itineraryID, HotelInformation hotelInformation) {
         itineraries.get(itineraryID).hotels.add(hotelInformation);
     }
+
     //Contact lameDuck and get some flights
-    public List<FlightInformation> getFlights(String origin, String destination, XMLGregorianCalendar departure){
+    public List<FlightInformation> getFlights(String origin, String destination, XMLGregorianCalendar departure) {
         GetFlightRequestType flightRequest = new GetFlightRequestType();
         flightRequest.setOrigin(origin);
         flightRequest.setDestination(destination);
         flightRequest.setFlightDate(departure);
         FlightInfoListType lameResponse = lameDuckPort.getFlights(flightRequest);
         ArrayList<FlightInformation> flightInfos = new ArrayList<>();
-        for (FlightInformationType type :lameResponse.getFlightInfo()){
+        for (FlightInformationType type : lameResponse.getFlightInfo()) {
             FlightInformation flightInfo = parseFlightInformationType(type);
             flightInfos.add(flightInfo);
         }
         return flightInfos; //TODO implement
     }
+
     //Contact NiceView and get som hotels
-    public List<HotelInformation> getHotels(String city, XMLGregorianCalendar arrival, XMLGregorianCalendar departure){
-        
+    public List<HotelInformation> getHotels(String city, XMLGregorianCalendar arrival, XMLGregorianCalendar departure) {
+
         return null; //TODO implement
     }
-    
+
     //Parsers----------------------
-     private FlightInformation parseFlightInformationType(FlightInformationType type) {
+    private FlightInformation parseFlightInformationType(FlightInformationType type) {
         FlightInformation flightInformation = new FlightInformation();
         flightInformation.bookingNumber = type.getBookingNumber();
         flightInformation.bookingState = parseState(type.getState());
@@ -160,18 +187,18 @@ public class DataSingleton {
         flightInformation.serviceName = type.getServiceName();
         return flightInformation;
     }
-    
-    private Itinerary.BookingState parseState(int stateInt){
-        switch (stateInt){
-                    case 0:
-                        return Itinerary.BookingState.PLANNING;
-                    case 1:
-                        return Itinerary.BookingState.PAID;
-                    case 2:
-                        return Itinerary.BookingState.CONFIRMED;
-                    case 3:
-                        return Itinerary.BookingState.CANCELLED;  
-            }
+
+    private Itinerary.BookingState parseState(int stateInt) {
+        switch (stateInt) {
+            case 0:
+                return Itinerary.BookingState.PLANNING;
+            case 1:
+                return Itinerary.BookingState.PAID;
+            case 2:
+                return Itinerary.BookingState.CONFIRMED;
+            case 3:
+                return Itinerary.BookingState.CANCELLED;
+        }
         return Itinerary.BookingState.PLANNING;
     }
 
@@ -187,10 +214,10 @@ public class DataSingleton {
     }
 
     private BookFlightRequestType createRequestFromFlightInfo(FlightInformation flight, CreditCardInfo creditCard) {
-      BookFlightRequestType request = new BookFlightRequestType();
-       request.setBookingNumber(flight.bookingNumber);
-       request.setCreditcardInfo(parseFlightCreditCardInfo(creditCard));
-       return request;
+        BookFlightRequestType request = new BookFlightRequestType();
+        request.setBookingNumber(flight.bookingNumber);
+        request.setCreditcardInfo(parseFlightCreditCardInfo(creditCard));
+        return request;
     }
 
     private CreditCardInfoType parseFlightCreditCardInfo(CreditCardInfo creditCard) {
@@ -213,5 +240,4 @@ public class DataSingleton {
         return hotelCreditCard;
     }
 
-   
 }
