14bda77bd899c3f36493dcb49e69f72f4d6391bf
Christian <chrbudtz@gmail.com>
2015-11-28 12:52:40 +0100

    GetItineraries done

--- TravelGoodREST/src/java/ws/group15/ItineraryResource.java
+++ TravelGoodREST/src/java/ws/group15/ItineraryResource.java
@@ -37,24 +37,32 @@ import ws.group15.dto.Itinerary;
  */
 @Path("itineraries")
 public class ItineraryResource {
-    
+    public final String baseUri= "http://localhost:8080/TravelGoodREST/"; //Desciption file
+    public final String webresourceUrl = baseUri + "webresources/";
     @Context
     UriInfo uriInfo;
     
-    //Base request 
+    //Base request - Not specified in requirements - made for completeness and experience
+    //Only allowed actions without an itinerary ID is get flights, get hotels and post itineraries
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response getItineraries(){
         UriBuilder ub = uriInfo.getAbsolutePathBuilder();
-        String url = ub.build().toString();
+        UriBuilder flightsUB = UriBuilder.fromResource(FlightsResource.class);
+        String flightsURI = webresourceUrl + flightsUB.build().toString();
+        UriBuilder hotelUB = UriBuilder.fromResource(HotelsResource.class);
+        String hotelsURI = webresourceUrl + hotelUB.build().toString();
+        String url = ub.build().toString(); //itineraryResource Link
         List<Itinerary> it = DataSingleton.getInstance().getItineraries();
         GenericEntity<List<Itinerary>> gen = new GenericEntity<List<Itinerary>>(it){};
         Response r = Response.ok(gen)
-                .link(url, POST)//Only allowed next action is to obtain some ID by creating an itinerary
+                .link(url, baseUri + "index.html#POSTItinerary")//Create new itinerary
+                .link(flightsURI, baseUri + "index.html#GETflights") //Get flights
+                .link(hotelsURI, baseUri + "index.html#GEThotels") // Get hotels
                 .build();
         return r;
     }
-    
+    //Create new itinerary - returns a link to resource
     @POST
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
     public Response createItinerary(){
@@ -62,7 +70,7 @@ public class ItineraryResource {
         Itinerary it = DataSingleton.getInstance().createItinerary();
         URI uri = ub.path(it.id).build();
         Response r = Response.created(uri)
-                .link(uri, "itineraty resource") //TODO: Figure out what to put in rel...
+                .link(uri, baseUri + "/index.html#getItinerary") //TODO: Figure out what to put in rel...
                 .link(uri.toString()+ "/status/cancel", "status")
                 .link(ub.clone().path("flights").build(), "Flights resource")
                 .link(ub.clone().path("hotels").build(), "Hotel resource")
