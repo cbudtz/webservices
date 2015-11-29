a9b779d52153e73e804499d2b6309c184da5eda6
Krish <Krish@krisha>
2015-11-29 16:20:24 +0100

    LinkBuilder....

---
+++ TravelGoodREST/src/java/ws/group15/LinkBuilder.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.group15;
+
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Kristin
+ */
+public class LinkBuilder {
+    public static final String baseUri = "http://localhost:8080/TravelGoodREST/";
+    public static final String webresourceUrl = baseUri + "webresources/";
+    public static final String hotelResource = webresourceUrl + "hotels";
+    public static final String flightsResource = webresourceUrl + "flights";
+    
+    public static Response.ResponseBuilder addCreateLink(Response.ResponseBuilder r) {
+        return r.link(baseUri, webresourceUrl);
+    }
+    
+    /**
+     * Builds a ResponseBuilder object containing links associated with the confirmed state
+     * @param r ResponseBuilder object which needs to have links applied to it
+     * @return ResponseBuilder object with links for planning state
+     */
+    public static Response.ResponseBuilder getAllPlanningLinks(Response.ResponseBuilder r) {
+        return r.link(hotelResource, baseUri + "index.html#GEThotels")
+                .link(flightsResource, baseUri + "index.html#GETflights")
+                .link(hotelResource, baseUri + "index.html#PUTitineraryHotels")
+                .link(flightsResource, baseUri + "index.html#PUTitineraryFlights")
+                .link(webresourceUrl, baseUri + "index.html#getItinerary")
+                .link(webresourceUrl + "state", baseUri + "index.html#putState");
+    }
+    
+    /**
+     * Builds a ResponseBuilder object containing links associated with the confirmed state
+     * @param r ResponseBuilder object which needs to have links applied to it
+     * @param resourceId Itinerary id
+     * @return ResponseBuilder object with links for planning state
+     */
+    public static Response.ResponseBuilder getPaidLinks(Response.ResponseBuilder r, String resourceId) {
+        return r.link(webresourceUrl + resourceId + "/state", baseUri + "index.html#putState")
+                .link(webresourceUrl + resourceId, baseUri + "index.html#getItinerary");
+    }
+    
+    /**
+     * Builds a ResponseBuilder object containing links associated with the confirmed state
+     * @param r
+     * @param resourceId
+     * @return 
+     */
+    public static Response.ResponseBuilder getConfirmedLinks(Response.ResponseBuilder r, String resourceId) {
+        return r.link(webresourceUrl + resourceId, baseUri + "index.html#getItinerary");
+    }
+    
+    /**
+     * Builds a ResponseBuilder object containing links associated with the confirmed state
+     * @param r ResponseBuilder object which needs to have links applied to it
+     * @param resourceId Itinerary id
+     * @return ResponseBuilder object with links for canceled state
+     */
+    public static Response.ResponseBuilder getCanceledLinks(Response.ResponseBuilder r, String resourceId) {
+        return r.link(webresourceUrl + resourceId, baseUri);
+    }
+}
