d5f20808752eb646f5ff1337af70a4e32248ba1a
Christian <chrbudtz@gmail.com>
2015-11-29 17:17:03 +0100

    Cleanup in code

--- TravelGoodREST/src/java/ws/group15/ItineraryResource.java
+++ TravelGoodREST/src/java/ws/group15/ItineraryResource.java
@@ -6,26 +6,16 @@
 package ws.group15;
 
 import java.net.URI;
-import java.util.AbstractList;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.DELETE;
-import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.GenericEntity;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
