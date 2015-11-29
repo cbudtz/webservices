22ba01cf5022a3de020bdd34f962896cb7d1b93c
Christian <chrbudtz@gmail.com>
2015-11-29 17:59:12 +0100

    minor fix in data singleton

---
+++ RestTest/test/TestAll.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Christian
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({P2.class, C2.class, C1.class, P1.class, B.class})
+public class TestAll {
+    
+}
