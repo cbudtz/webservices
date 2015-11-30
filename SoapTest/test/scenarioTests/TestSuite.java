6233b9d2a0d03f7bd67ef759c49c8a975cc50ff3
Rúni Egholm Vørmadal <runivormadal@outlook.com>
2015-11-30 10:13:27 +0100

    Merge origin/master
    Conflicts:
    	RestTest/test/ws/g15/dto/Conv.java

---
+++ SoapTest/test/scenarioTests/TestSuite.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scenarioTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Runi
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({scenarioTests.C1.class, scenarioTests.C2.class, scenarioTests.P2.class, scenarioTests.B.class, scenarioTests.P1.class})
+public class TestSuite {
+    
+}
